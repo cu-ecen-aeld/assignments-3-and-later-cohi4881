a3541589053077688193d57236cc8222d7801c9d
cohi4881 <cole.hinton@colorado.edu>
2022-10-04 23:15:07 -0400

    assignment 1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,7 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char * username_function = my_username();
+    char * username_txt = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(username_txt, username_function,"NOT EQUAL, please fix me!");
 }
